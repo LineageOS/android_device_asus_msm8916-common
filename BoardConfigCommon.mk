aa204b2a99dc1d306725e8eb4702eaaa382c8791
Adrian DC <radian.dc@gmail.com>
2018-02-18 14:35:54 +0100

    shim: Split TARGET_LD_SHIM_LIBS on multiple lines
     * Follows the vendor/lineage commit:
        "lineage: soong: replace space with colon on TARGET_LD_SHIM_LIBS"
        From: Stricted <info@stricted.net>
        Date: Sat, 17 Feb 2018 17:28:52 +0100
        Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf
    
    Change-Id: Ibfbf2a8025183c138ac9c0719a9352620f2c839c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -169,7 +169,10 @@ BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 TARGET_USE_SDCLANG := true
 
 # Shims
-TARGET_LD_SHIM_LIBS := /system/vendor/lib64/lib-imscamera.so|libshims_camera.so:/system/vendor/lib64/libflp.so|libshims_flp.so:/system/vendor/lib64/libizat_core.so|libshims_get_process_name.so
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib64/lib-imscamera.so|libshims_camera.so \
+    /system/vendor/lib64/libflp.so|libshims_flp.so \
+    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so
 
 # Tap-to-Wake
 TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/i2c-5/5-0038/dclick_mode"
