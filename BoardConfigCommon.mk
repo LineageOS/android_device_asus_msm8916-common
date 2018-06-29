0a6eec635fa7f21eb3678e79a2605bae4431c04a
Aayush Gupta <aayushgupta219@gmail.com>
2018-06-29 20:03:42 +0530

    msm8916-common: Remove SDCLANG
    *Not used by LineageOS-15.1
    This reverts commit f1c3c163f800916751e3a48163fdc9c3e53f5ede & 9e44573bc799ca97002cedf0a1a4a42bf2eeb661
    
    Change-Id: Ie26b011c78ececc8f6ca1aa4dd5ea0b92e82d03f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,9 +171,6 @@ include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
-# SDCLANG
-TARGET_USE_SDCLANG := true
-
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/lib-imscamera.so|libshims_camera.so \
